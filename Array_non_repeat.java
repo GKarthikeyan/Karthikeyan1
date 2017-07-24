e0d04c50296dad5bd2bb1b6f399f0b7b3edd1083
GKarthikeyan <karthikeyanvis1996@gmail.com>
2017-07-24 18:01:26 +0530

    Update Array_non_repeat.java

--- Array_non_repeat.java
+++ Array_non_repeat.java
@@ -3,19 +3,16 @@ import java.util.*;
 
 
 public class Array_non_repeat{
-    public static void main(String[] args) 
+     public static void main(String[] args) 
     {
-        int n, flag = 0, count = 0;
+        int n, flag = 0, count = 0,l=0;
         Scanner s = new Scanner(System.in);
-        System.out.print("Enter no. of elements you want in array:");     
+        System.out.print("Enter no. of elements you want in array:");   
         n = s.nextInt();
         int a[] = new int[n];
         System.out.println("Enter all the elements:");
         for(int i = 0; i < n; i++)
-        {
             a[i] = s.nextInt();
-        }
-        System.out.print("Non repeated elements are:");
         for(int i = 0; i < n; i++)
         {
             for(int j = 0; j < n; j++)
@@ -23,20 +20,24 @@ public class Array_non_repeat{
                 if(i != j)
                 {
                     if(a[i] != a[j])
-                        flag = 1;
+                       flag = 1;
                     else
                     {
-                       flag = 0;
-                       break;
+                        flag = 0;
+                        break;
                     }
                 }
             }
             if(flag == 1)
             {
+            	l=1;
                 count++;
-                System.out.print(a[i]+" ");
+                System.out.print("Non repeated elements are:"+a[i]+" ");
             }
         }
         System.out.println("");
+        if(l!=1)
+       	 System.out.print("No unique element in array");
     }
+
 }
