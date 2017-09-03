dd12dafdabbbd93369593611310f618df123c413
GKarthikeyan <karthikeyanvis1996@gmail.com>
2017-09-03 19:49:40 +0530

    Create str_rev.java

---
+++ str_rev.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class str_rev
+{
+ public static void main(String ar[])
+ {
+  Scanner sc=new Scanner(System.in);
+  String s=sc.nextLine();
+  String s1[]=s.split(" ");
+  System.out.print(s1[1]+" "+s1[0]);
+ }
+}
